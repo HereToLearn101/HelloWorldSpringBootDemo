10ef8221d83dbab4dcbadfbf335de69f34f84bcd
Edgar Moso <tedismoso02@gmail.com>
2018-01-24 16:36:52 -0500

    updated branch2

--- helloworld/src/main/resources/static/index.js
+++ helloworld/src/main/resources/static/index.js
@@ -1,30 +1,23 @@
 (function () {
 
     new Vue({
-        el: '#app',
-        data: {
-            message: 'You have successfully applied vue.js'
-        }
-    });
-
-    new Vue({
-        el: '#app-2',
-        data: {
-            message: 'Testing testing!'
-        }
-    });
-
-    new Vue({
             el: "#app-3",
             methods: {
             greet: function (response) {
-                 this.$http.get('/hello/greeting?name=Jong').then(function(response) {
+                 this.$http.get('/hello/greeting').then(function(response) {
                  // response.data refers to the whole data.
                     alert(response.data.name)
                  })
                 }
             }
         });
+
+    new Vue({
+        el: 'example',
+        data: {
+            checkedNames: []
+        }
+    });
 
 })();
 
--- helloworld/target/classes/static/index.js
+++ helloworld/target/classes/static/index.js
@@ -18,13 +18,20 @@
             el: "#app-3",
             methods: {
             greet: function (response) {
-                 this.$http.get('/hello/greeting?name=Jong').then(function(response) {
+                 this.$http.get('/hello/greeting').then(function(response) {
                  // response.data refers to the whole data.
                     alert(response.data.name)
                  })
                 }
             }
         });
+
+    new Vue({
+        el: 'example',
+        data: {
+            checkedNames: []
+        }
+    });
 
 })();
 
